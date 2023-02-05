4d91712021d05b8d8db39f9364c32431aedd39c1
Alessandro Platania <48250169+alesplat@users.noreply.github.com>
2023-02-05 12:18:44 +0100

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -161,7 +161,7 @@ const config = {
       secondary: '#219aaf',
       accent: '#e8d03a',
       neutral: '#1B1818', //default #2A2730
-      'base-100': '#ffffff', //default #E3E3ED',
+      'base-100': '#E2F0FF', //default #E3E3ED',
       '--rounded-box': '1rem',  //default 3rem
       '--rounded-btn': '1rem',   //default 3rem
     },
